2ce3ff6fd1e896ba127b8787ad86b053c51606d4
Henry Jimenez <henryjmzx19@hotmail.com>
2025-03-20 14:32:58 -0600

    Agregar archivos de proyecto.

---
+++ HEJM20250320S1.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HEJM20250320S1.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
